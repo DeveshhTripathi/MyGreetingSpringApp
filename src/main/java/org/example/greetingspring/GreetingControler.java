c66811c31e98298eea50fc346119603fffa0513e
DeveshhTripathi <deveshht591@gmail.com>
2025-03-07 14:00:52 +0530

    Devesh Tripathi Search by id

--- src/main/java/org/example/greetingspring/GreetingControler.java
+++ src/main/java/org/example/greetingspring/GreetingControler.java
@@ -13,29 +13,17 @@ public class GreetingControler{
     public GreetingControler(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
-    @GetMapping("/{name}")
-    public Greeting getGreet(@PathVariable String name){
-        return new Greeting("Hello, "+name+"!");
-    }
-    @PostMapping("/post")
-    public Greeting postGreeting(@RequestBody Greeting greeting) {
-        return new Greeting("Received: " + greeting.getMessage());
-    }
-
-    @PutMapping("/update/{name}")
-    public Greeting putGreeting(@PathVariable String name, @RequestBody Greeting greeting) {
-        return new Greeting("Updated: " + name + " -> " + greeting.getMessage());
-    }
-
-    @DeleteMapping("/remove/{name}")
-    public Greeting deleteGreeting(@PathVariable String name) {
-        return new Greeting("Deleted: " + name);
-    }
 
     @PostMapping("/save")
     public Greeting saveGreeting(@RequestBody Greeting greeting) {
         return greetingService.saveGreeting(greeting);
     }
+
+    @GetMapping("/{id}")
+    public Greeting getGreetingById(@PathVariable Long id) {
+        return greetingService.getGreetingById(id);
+    }
+
 
 
 }
--- src/main/java/org/example/greetingspring/GreetingService.java
+++ src/main/java/org/example/greetingspring/GreetingService.java
@@ -15,9 +15,37 @@ public class GreetingService {
     public GreetingService(GreetingRepository greetingRepository) {
         this.greetingRepository = greetingRepository;
     }
+    @GetMapping("/{name}")
+    public Greeting getGreet(@PathVariable String name){
+        return new Greeting("Hello, "+name+"!");
+    }
+    @PostMapping("/post")
+    public Greeting postGreeting(@RequestBody Greeting greeting) {
+        return new Greeting("Received: " + greeting.getMessage());
+    }
+
+    @PutMapping("/update/{name}")
+    public Greeting putGreeting(@PathVariable String name, @RequestBody Greeting greeting) {
+        return new Greeting("Updated: " + name + " -> " + greeting.getMessage());
+    }
+
+    @DeleteMapping("/remove/{name}")
+    public Greeting deleteGreeting(@PathVariable String name) {
+        return new Greeting("Deleted: " + name);
+    }
 
     public Greeting saveGreeting(Greeting greeting) {
         return greetingRepository.save(greeting);
     }
+
+    public Greeting getGreetingById(Long id) {
+        Optional<Greeting> greeting = greetingRepository.findById(id);
+        return greeting.orElse(new Greeting("Greeting not found!"));
+    }
+
+
+
+
+
 
 }
